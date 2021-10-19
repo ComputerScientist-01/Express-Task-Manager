47b3e73749a082f864589f190cc450ad426888fb
ComputerScientist-01 <rathi406@gmail.com>
2021-10-19 23:10:16 +0530

    🏗️ Update routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router= express.Router();
-
 //import controllers here
+const {getAllTasks} = require('../controllers/tasks')
 
-router.route('/').get((req, res) => {
-    res.send('all items')
-})
+router.route('/').get(getAllTasks);
 
 
 module.exports = router;

b707f37041a1fd9d0c63e0464206ad8497bab084
ComputerScientist-01 <rathi406@gmail.com>
2021-10-20 14:02:43 +0530

    ✨ add routes to path

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,6 +9,7 @@ const {
 	deleteTask,
 } = require("../controllers/tasks");
 
-router.route("/").get(getAllTasks);
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;

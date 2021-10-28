c6fe915a925b670e45736ed4d9903388cb34c80d
ComputerScientist-01 <rathi406@gmail.com>
2021-10-28 19:05:26 +0530

    ⚡️ sync -> async getAllTasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,12 @@
-const getAllTasks = (req, res) => {
-	res.send("get all tasks");
+const Task = require("../models/tasks");
+
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json({ tasks });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
 const getTask = (req, res) => {

549c7a7edcebb3f526a0d2c9667dde783857aed4
ComputerScientist-01 <rathi406@gmail.com>
2021-11-04 15:28:23 +0530

    ⚡️ sync -> async delete

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -37,8 +37,19 @@ const updateTask = (req, res) => {
 	res.send("update task");
 };
 
-const deleteTask = (req, res) => {
-	res.send("delete task");
+const deleteTask = async (req, res) => {
+	try {
+		const { id: taskID } = req.params;
+		const task = await Task.findOneAndDelete({ _id: taskID });
+
+		if (!task) {
+			return res.status(404).json({ msg: `No Task with ${taskID}` });
+		}
+
+		res.status(200).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
 module.exports = {

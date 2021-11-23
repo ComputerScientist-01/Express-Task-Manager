46d48cc039878c38fd6767b8a46e8ce1ff0b562e
ComputerScientist-01 <rathi406@gmail.com>
2021-11-23 21:01:07 +0530

    ⚰️ remove unnecessary route

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.static("./public"));
 
-//routes
-app.get("/hello", (req, res) => {
-	res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;

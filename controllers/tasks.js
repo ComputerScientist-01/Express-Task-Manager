ffe6598e47ee0e7a8b80f1994b9c5a528d5262ea
ComputerScientist-01 <rathi406@gmail.com>
2021-11-23 20:06:12 +0530

    🐛 added validators for Patch

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -52,7 +52,10 @@ const updateTask = async (req, res) => {
 	try {
 		const { id: taskID } = req.params;
 
-		const task = await Task.findOneAndUpdate({ _id: taskID }, req.body);
+		const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+			new: True,
+			runValidators: True,
+		});
 
 		if (!task) {
 			return res.status(404).json({ msg: `No Task with ${taskID}` });
